c1e657a66338612fefabab158fe9e888729f3010
david <stone.dawei@gmail.com>
2015-03-22 23:12:10 +0800

    mid sub

--- cpkl.h
+++ cpkl.h
@@ -1,4 +1,14 @@
 #ifndef _CPKL_H_
 #define _CPKL_H_
 
+/*****************************************************************************
+ * Add some definition here.
+ *****************************************************************************/
+
+
+/*
+ * DON'T modify these include files.
+ */
+#include "cpkl_typedef.h"
+
 #endif
---
+++ cpkl_typedef.h
@@ -0,0 +1,15 @@
+#ifndef _CPKL_TYPEDEF_H_
+#define _CPKL_TYPEDEF_H_
+
+typedef	unsigned char				u8;
+typedef unsigned short				u16;
+typedef unsigned long				u32;
+typedef unsigned long long			u64;
+
+typedef char						s8;
+typedef short						s16;
+typedef int							s32;
+typedef long long					s64;
+
+
+#endif
